dc542bb4c4925807a05ea55f6de05f14faa075f2
book-reader-repo <bookreaderforgithub@gmail.com>
2026-09-11 22:05:39 -0800

    Create TocPage.xaml.cs

---
+++ Views/TocPage.xaml.cs
@@ -0,0 +1,44 @@
+using BookViewer.Models;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+
+namespace BookViewer.Views
+{
+    public partial class TocPage : ContentPage
+    {
+        private readonly string _bookFolder;
+        private readonly BookData _bookData;
+
+        public TocPage(string bookFolder, BookData bookData)
+        {
+            InitializeComponent();
+            _bookFolder = bookFolder;
+            _bookData = bookData;
+
+            TitleLabel.Text = "ENGLISH";
+            BookTitleLabel.Text = bookData.Title;
+
+            var units = new List<UnitData>(bookData.Units);
+            UnitsCollection.ItemsSource = units;
+        }
+
+        private async void OnBackTapped(object sender, System.EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void OnUnitTapped(object sender, System.EventArgs e)
+        {
+            if (((Grid)sender).BindingContext is UnitData unit)
+            {
+                var unitPage = new UnitPage(_bookFolder, _bookData, unit);
+                await Navigation.PushAsync(unitPage);
+            }
+        }
+
+        private void OnUnitSelected(object sender, SelectionChangedEventArgs e)
+        {
+            // handled by tap gesture
+        }
+    }
+}
