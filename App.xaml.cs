ed6810b53abec6e6307846e8186efcd88d2fbe85
book-reader-repo <bookreaderforgithub@gmail.com>
2026-09-11 22:06:41 -0800

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,10 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        MainPage = new MainPage();
+        MainPage = new NavigationPage(new Views.LibraryPage())
+        {
+            BarBackgroundColor = Colors.Transparent,
+            BarTextColor = Colors.White
+        };
     }
 }
